337e4cbe1dac8c36645f9a81fac802a8ffe40132
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2021-01-18 17:07:45 +0800

    deleted 1 line

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,7 +1,6 @@
 public class helloworld {
     public static void main(String[] args){
         System.out.println(args[0]);
-        System.out.println(args[1]);
         System.out.println(args[0].equals(args[1]));
     }
 
