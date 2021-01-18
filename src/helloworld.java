b8d6d8d3f65f532b9b080f82722e30be4dba1a30
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2021-01-18 17:08:42 +0800

    Update helloworld.java

--- src/helloworld.java
+++ src/helloworld.java
@@ -2,6 +2,7 @@ public class helloworld {
     public static void main(String[] args){
         System.out.println(args[0]);
         System.out.println(args[0].equals(args[1]));
+        //comment//
     }
 
 }
